a39ee9da45c1b7ad8e03c68a93e397693748b445
ssaranyaa <35294743+ssaranyaa@users.noreply.github.com>
2018-01-17 16:54:06 +0530

    alphabetornot.C

---
+++ alphabetornot.C
@@ -0,0 +1,12 @@
+#include<sidio.h>
+int main()
+{
+char c;
+printf("enter the character");
+scanf("%c",&c);
+if((c>='a'7&c<='z')//(c>='A'&&c<='Z'))
+printf("%c is an alphabet",c);
+else
+printf("%c is not an alphabet",c);
+return0;
+}
